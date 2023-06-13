38dab4759ec4612259b12185d25fe43d376aadbf
Marisi Romanillos <marisiromanillos@Marisis-MBP.lan>
2023-06-13 08:38:52 +0100

    change all the stateles class components to functions

--- src/components/Simpsons.jsx
+++ src/components/Simpsons.jsx
@@ -1,25 +1,49 @@
-import React, { Component } from "react";
+// import React, { Component } from "react";
+// import Character from "./Character";
+
+// class Simpsons extends Component {
+//   render() {
+//     const { simpsons } = this.props;
+
+//     return (
+//       <>
+//         {simpsons.map((item) => {
+//           return (
+//             <Character
+//               item={item}
+//               key={item.id}
+//               onDelete={this.props.onDelete}
+//               onLikeToggle={this.props.onLikeToggle}
+//             />
+//           );
+//         })}
+//       </>
+//     );
+//   }
+// }
+
+// export default Simpsons;
+
+import React from "react";
 import Character from "./Character";
 
-class Simpsons extends Component {
-  render() {
-    const { simpsons } = this.props;
+const Simpsons = (props) => {
+  const { simpsons } = props;
 
-    return (
-      <>
-        {simpsons.map((item) => {
-          return (
-            <Character
-              item={item}
-              key={item.id}
-              onDelete={this.props.onDelete}
-              onLikeToggle={this.props.onLikeToggle}
-            />
-          );
-        })}
-      </>
-    );
-  }
-}
+  return (
+    <>
+      {simpsons.map((item) => {
+        return (
+          <Character
+            item={item}
+            key={item.id}
+            onDelete={props.onDelete}
+            onLikeToggle={props.onLikeToggle}
+          />
+        );
+      })}
+    </>
+  );
+};
 
 export default Simpsons;
